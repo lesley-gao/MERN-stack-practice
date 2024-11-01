965bda5173810fcee275c12613694c29f6c54858
Lesley <glei164@aucklanduni.ac.nz>
2024-11-02 01:25:02 +1300

    server and database connected

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose"
+
+//a function to connect the database
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+        process.exit(1); //process code 1 represents exit with failure, 0 means success.
+    }
+} 
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+dotenv.config(); //configure environment variable
 
 const app = express(); //create the express server
 
 app.listen(5000, () => {
+    connectDB();
     console.log('Server started at http://localhost:5000');
 }) //start the server running
 
