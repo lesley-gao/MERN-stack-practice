cc0165e363a6c0e3b7d0dc22e9b33d3f7819b1fa
Lesley <glei164@aucklanduni.ac.nz>
2024-11-03 23:01:36 +1300

    add APIs for deleting products and getting all products

--- backend/server.js
+++ backend/server.js
@@ -13,24 +13,48 @@ const app = express(); //create the express server
 
 app.use(express.json()); //add a middleware, allowing use to accept JSON data in the req.body
 
-app.post("/products", async (req, res) =>{
+// get all products 
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find({}); // if we pass an emptry object, this means fetch all the products in the database
+        console.log(products);
+        res.status(200).json({ success: true, data: products })
+    } catch (error) {
+        console.log("error in fetching products:", error.message); //for debugging
+        res.status(500).json({ success: false, message: "Server Error" })
+    }
+});
+
+// post a new product
+app.post("/api/products", async (req, res) => {
     const product = req.body; //user will send this data
-    
-    if (!product.name || !product.price || !product.image){
-        return res.status(400).json({success: false, message: "Please provide all fields"});
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: "Please provide all fields" });
     } //if any of the required fields are not provides, return the 400 code and the warning message
 
     const newProduct = new Product(product);
-    try{
+    try {
         await newProduct.save();
-        res.status(201).json({success: true, data: newProduct});
-    } catch(error){
-        console.error("Error in Create product:", error.message);
-        res.status(500).json({success:false, message: "Server Error"});
+        res.status(201).json({ success: true, data: newProduct });
+    } catch (error) {
+        console.error("Error in Create product:", error.message); //for debugging
+        res.status(500).json({ success: false, message: "Server Error" });
     }
 })
 
-
+//delete a product
+app.delete("/api/products/:id", async (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product deleted" });
+    } catch (error) {
+        console.log("error in deleting the product:", error.message); //for debugging
+        res.status(404).json({ success: false, message: "Product not found" });
+    }
+})
 
 app.listen(5000, () => {
     connectDB();
