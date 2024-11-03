dc3ab9d5ec432494c8d91032b948c2979af33ef0
Lesley <glei164@aucklanduni.ac.nz>
2024-11-04 11:28:50 +1300

    add PORT variable

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import productRoutes from './routes/product.route.js';
 dotenv.config(); //configure environment variable
 
 const app = express(); //create the express server
+const PORT = process.env.PORT || 5000; //add" || 5000 " as a backup
 
 app.use(express.json()); //add a middleware, allowing use to accept JSON data in the req.body
 
@@ -13,5 +14,5 @@ app.use("/api/products", productRoutes); // when we vist "/api/products", we wil
 
 app.listen(5000, () => {
     connectDB();
-    console.log('Server started at http://localhost:5000');
+    console.log(`Server started at http://localhost:${PORT}`);
 }) //start the server running
